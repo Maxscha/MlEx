2b3b79515d4a3502c40078814a594059de6d22c1
maxscha <maxscha@mail.de>
2020-09-04 14:44:21 +0200

    Fixes gaussian outlier with 0 variance

--- lib/gaussian_outlier.ex
+++ lib/gaussian_outlier.ex
@@ -7,6 +7,10 @@ defmodule MlEx.GaussianOutlier do
     Enum.sum(Enum.map(l, fn x -> (x - m) * (x - m) end)) / Enum.count(l)
   end
 
+  def probability(x, mean, 0) do
+    if x == mean, do: 1, else: 0
+  end
+
   def probability(x, mean, variance) do
     first = 1 / :math.sqrt(2 * :math.pi() * variance)
     in_exp = -((x - mean) * (x - mean) / 2 * variance)
